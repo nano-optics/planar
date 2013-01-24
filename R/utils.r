d2dfc004abda2bb4e98cbfd115b42a833d9733d9
baptiste <baptiste.auguie@gmail.com>
2013-01-24 16:45:18 +1300

    cleanup utils

--- R/utils.r
+++ R/utils.r
@@ -18,3 +18,23 @@ Curry <- function (FUN, ...)
     .orig = list(...)
     function(...) do.call(FUN, c(.orig, list(...)))
 }
+
+##' Raman shift
+##'
+##' Raman shift conversion to absolute wavelength
+##' @title raman_shift
+##' @export
+##' @param wavelength wavelength (nm)
+##' @param shift Raman shift (cm-1)
+##' @param antistokes logical Stokes or Anti-Stokes
+##' @return wavelength of the Raman peak in nm
+##' @author Baptiste Auguie
+##' @examples
+##' raman.shift(wavelength=200)
+raman_shift <- function(wavelength = 500, shift = 1000, stokes = TRUE){
+
+  lambdaR <- 1 / shift * 1e7
+
+  if(stokes) 1 / (1/lambda - 1/lambdaR) else 1 / (1/lambda + 1/lambdaR)
+
+}

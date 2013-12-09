9245a7cce7f22a9c14e4df6fb2cf6d54952e7888
baptiste <baptiste@baptistes-MacBook-Pro-2.local>
2013-12-09 18:12:26 -0300

    successful comparison with plane waves

--- R/gaussian.r
+++ R/gaussian.r
@@ -1,4 +1,31 @@
 
+integration_points_disk <- function(Nquad = 30, 
+                                    min=c(0, 0), max=c(1, 2*pi),
+                                    method="GL",  init=TRUE){
+  
+  method <- match.arg(method)
+  rho1=min[1]; rho2=max[1]; theta1=min[2]; theta2=max[2]; 
+  C1 = (rho2 - rho1) / 2;  D1 = (rho1 + rho2) / 2;
+  C2 = (theta2 - theta1) / 2; D2 = (theta1 + theta2) / 2;
+  
+  rndN <- ceiling(sqrt(Nquad))
+  GL_rho <- statmod::gauss.quad(rndN)
+  GL_theta <- statmod::gauss.quad(rndN)
+  
+  rho = GL_rho$nodes*C1 + D1  
+  theta = GL_theta$nodes*C2 + D2
+  
+  # grid of params
+  grid <- expand.grid(rho=rho, theta=theta)
+  # corresponding weights for 2D quadrature
+  weights <- expand.grid(rho=GL_rho$weights, theta=GL_theta$weights)
+  # combine the weigths for each point; grid$rho comes from the Jacobian in the integral
+  weights <- C1 * C2 * grid$rho * weights$rho * weights$theta
+  
+  return(list(grid=as.matrix(grid), weights=weights))
+}  
+
+
 ##' Electric field from the transmission of a gaussian beam at a planar interface
 ##'
 ##' Integration is performed over a spectrum of incident plane waves using integrand_gb
@@ -8,7 +35,7 @@
 ##' @param z position
 ##' @param wavelength wavelength
 ##' @param alpha beam incident angle
-##' @param psi beam polarisation angle
+##' @param theta beam polarisation angle
 ##' @param w0 beam waist radius
 ##' @param ni incident medium (prism) refractive index
 ##' @param no outer medium (substrate) refractive index
@@ -21,24 +48,23 @@
 ##' @family gaussian_beam
 ##' @author Baptiste Auguie
 gaussian_field <- function(x=1, y=1, z=1, wavelength=500, alpha = 15*pi/180, psi=0, 
-                           w0=1e4, ni=1.5, no=1, nl=no, d=0,
-                           cutoff = min(1, sqrt(3*4)/(w0*Re(2*pi/wavelength*ni))),
-                           maxEval = 300){
+                           w0=1e4, ni=1.5, no=1.0, nl=no, d=0,
+                           cutoff = min(1, 3*wavelength/(ni*pi*w0)), # integrand ~ exp(-3^2)
+                           maxEval = 3000){
   
+#   message(cutoff)
   k <- 2*pi/wavelength
   ki <- k*ni
-  res <- adaptIntegrate(gaussian$integrand_gb,
-                        lowerLimit=c(0, 0), # rho in [0,1], angle in [0,2*pi]
-                        upperLimit=c(cutoff, 2*pi), # exp(-3) is tiny
-                        fDim = 6, tol = 1e-04,
-                        maxEval = maxEval,
-                        r2 = c(x, y, z), ki=ki, psi=psi, alpha=alpha,
-                        w0=w0, ni=ni, no=no, nl=nl, d=d)$integral
+  res <- cubature::adaptIntegrate(gaussian$integrand_gb,
+                                  lowerLimit=c(0, 0), # rho in [0,1], angle in [0,2*pi]
+                                  upperLimit=c(cutoff, 2*pi), 
+                                  fDim = 6, tol = 1e-04,
+                                  maxEval = maxEval,
+                                  r2 = c(x, y, z), ki=ki, psi= psi, alpha=alpha,
+                                  w0=w0, ni=ni, no=no, nl=nl, d=d)$integral
   
   E <- complex(real = res[1:3], imag=res[4:6])
   Re(crossprod(E, Conj(E)))
   
 }
-
-
 
--- src/gaussian_beam.cpp
+++ src/gaussian_beam.cpp
@@ -94,18 +94,31 @@ arma::colvec integrand_gb(const colvec& rt, const colvec& r2, const double ki, \
     double delta, rho, theta, sx, sy;
     cx_double i = cx_double(0,1), a, pw, ko, kl;
   
-    cx_colvec ei1(3), ei2(3), ei2p(3), eo2p(3), Eo2(3);
+    cx_colvec Eo2 = arma::zeros<arma::cx_colvec>(3); // result
+    cx_colvec ei1(3), ei2(3), ei2p(3), eo2p(3);
     colvec ki1(3), ki2(3), ki2p(3);
     cx_colvec ko2(3), ko2p(3), kl2p(3);
     mat Ry(3, 3), Rz(3, 3), Rzi(3, 3);
 
     cx_double ni2 = ni*ni, no2 = no*no, nl2=nl*nl, nratio2 = ni2/no2;
+    ko = no / ni * ki; // outer medium
+    kl = nl / ni *ki; // layer
     cx_double tp, ts, ap, bp;
 
     // change of variables from polar coordinates
     rho = rt(0); theta = rt(1);
     sx = rho * cos(theta);
     sy = rho * sin(theta);
+
+    double root = 1.0 - rho*rho;
+
+    if( root < 0.0) // only prop. waves are considered
+      {    
+	colvec Er = real(Eo2);
+	colvec Ei = imag(Eo2);
+	colvec res = join_cols(Er, Ei);
+	return(res); 
+      }
 
     // work out kz component
     ki1(0) = ki*sx;
@@ -122,6 +135,9 @@ arma::colvec integrand_gb(const colvec& rt, const colvec& r2, const double ki, \
     Ry = rotation_y(alpha);
     ki2 = Ry * ki1;
     ei2 = Ry * ei1;
+    ko2(0) = ki2(0);
+    ko2(1) = ki2(1);
+    ko2(2) = sqrt(ko*ko - ko2(0)*ko2(0) - ko2(1)*ko2(1));
     
     // to frame F2p
     delta = asin(ki2(1) / sqrt(ki2(0)*ki2(0) + ki2(1)*ki2(1)));
@@ -131,18 +147,16 @@ arma::colvec integrand_gb(const colvec& rt, const colvec& r2, const double ki, \
     ei2p = Rz * ei2;
   
     // transmission through interface
-    ko = no / ni * ki; // outer medium
-    kl = nl / ni *ki; // layer
     kl2p(0) = ki2p(0); 
     kl2p(1) = ki2p(1);
     kl2p(2) = sqrt(kl*kl - kl2p(0)*kl2p(0) - kl2p(1)*kl2p(1));
     ko2p(0) = ki2p(0); 
     ko2p(1) = ki2p(1);
     ko2p(2) = sqrt(ko*ko - ko2p(0)*ko2p(0) - ko2p(1)*ko2p(1));
-    // eo2p = evanescent_field(ei2p, ki2p, ko2p, ni, no);
 
     // Fresnel coefficients
 
+    // single-interface (for testing)
     // tp = 2.0 * no2 * ki2p(2) / (no2*ki2p(2) + ni2*ko2p(2));
     // ts = 2.0 * ki2p(2) / (ki2p(2) + ko2p(2));
 
@@ -182,9 +196,8 @@ arma::colvec integrand_gb(const colvec& rt, const colvec& r2, const double ki, \
     eo2p(2) = nratio2 * tp * ei2p(2);
 
     // incident plane wave
-    pw = exp(i*(ki2(0)*r2(0) + ki2(1)*r2(1) + sqrt(ko*ko - ki2(0)*ki2(0) +0.0*i)*r2(2)));
+    pw = exp(i*(ko2(0)*r2(0) + ko2(1)*r2(1) + ko2(2)*r2(2)));
 
-    // Eo2 = rho * a * pw  * Rzi * eo2p;
     Eo2 = rho*ki*ki * a * pw  * Rzi * eo2p;
 
     // join real and imaginary part in 6-vector for cubature::adaptIntegrate
@@ -195,8 +208,140 @@ arma::colvec integrand_gb(const colvec& rt, const colvec& r2, const double ki, \
     return (res);
   }
 
-RCPP_MODULE(gaussian){
-  Rcpp::function( "integrand_gb", &integrand_gb,					\
-	    "Integrand for the transmitted field under gaussian illumination" ) ;
+arma::cx_colvec integrand_gb2(const rowvec& rt, const rowvec& r2, const double ki, \
+			  const double psi, const double alpha, const double w0, \
+			  const double ni, const double no, const cx_double nl, const double d)
+  {
 
+    double delta, rho, beta, theta, sx, sy;
+    cx_double i = cx_double(0,1), a, pw, ko, kl;
+    cx_colvec Eo2 = arma::zeros<arma::cx_colvec>(3); // result
+    cx_colvec ei1(3), ei2(3), ei2p(3), eo2p(3);
+    colvec ki1(3), ki2(3), ki2p(3);
+    cx_colvec ko2(3), ko2p(3), kl2p(3);
+    mat Ry(3, 3), Rz(3, 3), Rzi(3, 3);
+
+    cx_double ni2 = ni*ni, no2 = no*no, nl2=nl*nl, nratio2 = ni2/no2;
+    ko = no / ni * ki; // outer medium
+    kl = nl / ni *ki; // layer
+    cx_double tp, ts, ap, bp;
+
+    // change of variables from polar coordinates
+    beta = rt(0); theta = rt(1);
+    rho = beta * 8.0 / (ki * w0);
+    sx = rho * cos(theta);
+    sy = rho * sin(theta);
+    double root = ki*ki - rho*rho;
+    // cout << root << endl;
+    if( root < 0) // only prop. waves are considered
+      return(Eo2); 
+
+    // work out kz component
+    ki1(0) = sx;
+    ki1(1) = sy;
+    ki1(2) = sqrt(root); 
+
+    // incident field polarisation and distribution
+    ei1 = incident_field(psi);
+    a = w0*w0 / (4*datum::pi) * exp(-(ki1(0)*ki1(0) + ki1(1)*ki1(1))*(w0*w0/4));
+
+    // rotations of incident field
+
+    // to frame F2
+    Ry = rotation_y(alpha);
+    ki2 = Ry * ki1;
+    ei2 = Ry * ei1;
+    ko2(0) = ki2(0);
+    ko2(1) = ki2(1);
+    ko2(2) = sqrt(ko*ko - ko2(0)*ko2(0) - ko2(1)*ko2(1));
+    
+    // to frame F2p
+    delta = asin(ki2(1) / sqrt(ki2(0)*ki2(0) + ki2(1)*ki2(1)));
+    Rz = rotation_z(delta);
+    Rzi = rotation_z(-delta);
+    ki2p = Rz * ki2;
+    ei2p = Rz * ei2;
+  
+    // transmission through interface
+    kl2p(0) = ki2p(0); 
+    kl2p(1) = ki2p(1);
+    kl2p(2) = sqrt(kl*kl - kl2p(0)*kl2p(0) - kl2p(1)*kl2p(1));
+    ko2p(0) = ki2p(0); 
+    ko2p(1) = ki2p(1);
+    ko2p(2) = sqrt(ko*ko - ko2p(0)*ko2p(0) - ko2p(1)*ko2p(1));
+
+    // Fresnel coefficients
+
+    // single-interface (for testing)
+    // tp = 2.0 * no2 * ki2p(2) / (no2*ki2p(2) + ni2*ko2p(2));
+    // ts = 2.0 * ki2p(2) / (ki2p(2) + ko2p(2));
+
+    // p-pol
+    ap = ki2p(2) / ni2;
+    bp = kl2p(2) / nl2;
+    arma::cx_double rp01 = (ap - bp) / (ap + bp);
+    arma::cx_double tp01 = 2.0 * ap / (ap + bp);
+    
+    ap = kl2p(2) / nl2;
+    bp = ko2p(2) / no2;
+    arma::cx_double rp12 = (ap - bp) / (ap + bp);
+    arma::cx_double tp12 = 2.0 * ap / (ap + bp);
+
+    // s-pol
+
+    ap = ki2p(2);
+    bp = kl2p(2);
+    arma::cx_double rs01 = (ap - bp) / (ap + bp);
+    arma::cx_double ts01 = 2.0 * ap / (ap + bp);
+    
+    ap = kl2p(2);
+    bp = ko2p(2);
+    arma::cx_double rs12 = (ap - bp) / (ap + bp);
+    arma::cx_double ts12 = 2.0 * ap / (ap + bp);
+ 
+    arma::cx_double phase1 =  exp(i*d*kl2p(2));
+    arma::cx_double phase2 =  exp(2.0*i*d*kl2p(2));
+   
+    // rp  = ( rp01 + rp12%phase2 ) / ( 1 + rp01%rp12%phase2 );
+    // rs  = ( rs01 + rs12%phase2 ) / ( 1 + rs01%rs12%phase2 );
+    tp  = ( tp01 * tp12 * phase1 ) / ( 1.0 + rp01 * rp12 * phase2 );
+    ts  = ( ts01 * ts12 * phase1 ) / ( 1.0 + rs01 * rs12 * phase2 );
+    
+    eo2p(0) = nratio2 * tp * ko2p(2)/ki2p(2) * ei2p(0);
+    eo2p(1) = ts * ei2p(1);
+    eo2p(2) = nratio2 * tp * ei2p(2);
+
+    // incident plane wave
+    pw = exp(i*(ko2(0)*r2(0) + ko2(1)*r2(1) + ko2(2)*r2(2)));
+    
+    Eo2 = ki*ki * a * 8.0 / (ki * w0) * pw  * Rzi * eo2p;
+    
+    return (Eo2);
+  }
+
+// [[Rcpp::export]]
+arma::cx_mat integral_gb(const mat& rt, const colvec& weights, const mat& r2, const double ki, \
+			 const double psi, const double alpha, const double w0, \
+			 const double ni, const double no, const cx_double nl, const double d)
+{
+  const int N = r2.n_rows;
+  const int Nquad = rt.n_rows;
+  cx_mat field = arma::zeros<arma::cx_mat>(N, 3);
+  cx_colvec tmp = arma::zeros<arma::cx_colvec>(3);
+  int ii=0, jj=0;
+  for(ii=0; ii<N; ii++){
+    for(jj=0; jj<Nquad; jj++){
+      tmp = tmp + weights(jj) * integrand_gb2(rt.row(jj), r2.row(ii), ki, psi, alpha, w0, ni, no, nl, d);
+    } 
+  field.row(ii) = tmp.st();
+  }
+  return (field);
+}
+
+RCPP_MODULE(gaussian){
+  Rcpp::function( "integrand_gb", &integrand_gb,			\
+		  "Integrand for the transmitted field under gaussian illumination" ) ;
+  Rcpp::function( "integral_gb", &integral_gb,				\
+		  "Integral for the transmitted field under gaussian illumination" ) ;
+  
 }
